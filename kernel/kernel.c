f0d680df62f5caf72137ee5a40f2e24ed299fa46
Aren Elchinyan <a2.dev@yandex.ru>
2025-01-09 18:04:39 +0300

    Add status check

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,10 +5,19 @@ int multiboot_init(uint64_t *addr, uint32_t magic);
 
 void kernel_main64(uint64_t *multiboot, uint32_t magic, void *esp, uint64_t base) {
 	serial_init( );
-	multiboot_init(multiboot, magic);
-	serial_write_byte('[');
-	serial_write_byte('O');
-	serial_write_byte('K');
-	serial_write_byte(']');
+	int status = multiboot_init(multiboot, magic);
+
+	if (status) {
+		serial_write_byte('[');
+		serial_write_byte('O');
+		serial_write_byte('K');
+		serial_write_byte(']');
+	} else {
+		serial_write_byte('[');
+		serial_write_byte('E');
+		serial_write_byte('R');
+		serial_write_byte('R');
+		serial_write_byte(']');
+	}
 	for (;;) {}
 }
